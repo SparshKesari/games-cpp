86382d91f071ba29b8858397499158fe42fd8214
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2021-06-09 17:54:26 +0530

    draw function added
    Signed-off-by: Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>

--- tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe.cpp
@@ -1,19 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-char martrix[3][3]={'1','2','3','4','5','6','7','8','9'};
-
-void Draw(){
-for(int i = 0; i<3;i++)
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
+void Draw()
 {
-	for(int j = 0; j<3;j++)
+	system("cls");
+	cout << "Tic Tac Toe v1.0" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
 		{
-			cout<<matrix[i][j]<<" ";
+			cout << matrix[i][j] << " ";
 		}
-}
+		cout << endl;
+	}
 }
 
-int main(){
-	
+int main()
+{
+	Draw();
+	while (1)
+	{
+		Input();
+		Draw();
+		if (Win() == 'X')
+		{
+			cout << "X wins!" << endl;
+			break;
+		}
+		else if (Win() == 'O')
+		{
+			cout << "O wins!" << endl;
+			break;
+		}
+		TogglePlayer();
+	}
+	system("pause");
 	return 0;
 }
