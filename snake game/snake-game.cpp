bcc5ab4bb41183c22e0c20f407ca5d0ded4f3738
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2021-06-09 19:20:40 +0530

    input finction
    Signed-off-by: Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>

--- snake game/snake-game.cpp
+++ snake game/snake-game.cpp
@@ -72,7 +72,27 @@ void Draw()
 }
 void Input()
 {
-    //input function
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 void Logic()
 {
